b399a83e0cdd119895591f03dd53071649f22205
vivianogo <ogoogbuanu@gmail.com>
2022-06-02 17:17:28 +0100

    My First Java program

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        //My first java program
+        System.out.println("Hello! Ogochukwu your first Java Program");
+
+        //Variable declaration
+        int num = 1;
+        char grade = 'A';
+        String name = "Ogochukwu";
+        System.out.println(num);
+        System.out.println(grade);
+        System.out.println(name);
+
+        //Length of an array, number of element in the array
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+
+    }
+}
